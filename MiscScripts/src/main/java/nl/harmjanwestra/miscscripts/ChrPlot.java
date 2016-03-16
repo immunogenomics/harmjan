47c543ffb28cdefbf7a5ec841001b15dbc25033f
Harm-Jan Westra <westra.harmjan@outlook.com>
2016-03-16 17:24:03 -0400

    Minor changes

--- MiscScripts/src/main/java/nl/harmjanwestra/miscscripts/ChrPlot.java
+++ MiscScripts/src/main/java/nl/harmjanwestra/miscscripts/ChrPlot.java
@@ -2,9 +2,13 @@ package nl.harmjanwestra.miscscripts;
 
 import com.itextpdf.text.DocumentException;
 import nl.harmjanwestra.utilities.graphics.ChromosomePlot;
+import umcg.genetica.io.text.TextFile;
+import umcg.genetica.text.Strings;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by hwestra on 8/26/15.
@@ -12,10 +16,18 @@ import java.io.IOException;
 public class ChrPlot {
 
 	public static void main(String[] args) {
+//		ChrPlot c = new ChrPlot();
+//		try {
+//			c.reannotateVariants();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		System.exit(-1);
+
 		String cytoband = "/Data/Ref/Annotation/UCSC/cytoBand.txt";
 		ChromosomePlot plot = null;
 		try {
-			plot = new ChromosomePlot("/Data/tmp/chrplot.pdf", 1200, 1200);
+			plot = new ChromosomePlot("/Data/tmp/chrplot-tb.pdf", 1200, 1200);
 			plot.setMargin(200);
 //			String[] gfffiles = new String[]{
 //					"/Data/ImmunoBase//Hs_GRCh38-AA-assoc_genesGFF",
@@ -48,7 +60,7 @@ public class ChrPlot {
 			};
 
 			gfffiles = new String[]{
-					"/Data/tmp/bmi.txt"};
+					"/Data/tmp/tb-edit.txt"};
 			String sequencedRegionFile = "/Data/Projects/2014-FR-Reseq/2015-finalRun/2015-04-07-Analysis/2015-03-30-allRegionsWith50PercentOfSamplesAbove20x.bed";
 			plot.plot(cytoband, gfffiles, false, null);
 //			plot.heatmap(gfffiles, true);
@@ -59,6 +71,74 @@ public class ChrPlot {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+
+	}
+
+	public void reannotateVariants() throws IOException {
+		String filein = "/Data/tmp/tb.txt";
+		String fileout = "/Data/tmp/tb-edit.txt";
+		String dbsnpvcf = "/Data/Projects/2014-FR-Reseq/GATKResources/dbsnp_138.b37.vcf.gz";
+
+
+		HashMap<String, String> variants = new HashMap<String, String>();
+		TextFile tf = new TextFile(filein, TextFile.R);
+		String[] elems = tf.readLineElems(TextFile.tab);
+		while (elems != null) {
+			if (elems.length == 10) {
+				variants.put(elems[9], null);
+			}
+			elems = tf.readLineElems(TextFile.tab);
+		}
+		tf.close();
+
+		System.out.println(variants.size() + " variants loaded");
+
+		TextFile vcf = new TextFile(dbsnpvcf, TextFile.R);
+
+		System.out.println("parsing DBSNP VCF: " + dbsnpvcf);
+		String[] lineElems = vcf.readLineElems(TextFile.tab);
+		int ln = 0;
+		while (lineElems != null) {
+
+			if (lineElems[0].startsWith("#")) {
+				// header
+			} else {
+
+				String rs = lineElems[2];
+				if (variants.containsKey(rs)) {
+					variants.put(rs, lineElems[1]);
+				}
+			}
+
+			ln++;
+
+			if (ln % 2500000 == 0) {
+				System.out.println(ln + " positions parsed...");
+			}
+			lineElems = vcf.readLineElems(TextFile.tab);
+		}
+		vcf.close();
+
+		TextFile tfout = new TextFile(fileout, TextFile.W);
+		tf = new TextFile(filein, TextFile.R);
+		elems = tf.readLineElems(TextFile.tab);
+		while (elems != null) {
+			if (elems.length == 10) {
+				String variant = elems[9];
+				String pos = variants.get(variant);
+
+				if (pos != null) {
+					elems[3] = "" + (Integer.parseInt(pos) - 1);
+					elems[4] = pos;
+				}
+				tfout.writeln(Strings.concat(elems, Strings.tab));
+			}
+			elems = tf.readLineElems(TextFile.tab);
+		}
+		tf.close();
+		tfout.close();
+
 
 	}
 }
--- Utilities/src/main/java/umcg/genetica/console/ProgressBar.java
+++ Utilities/src/main/java/umcg/genetica/console/ProgressBar.java
@@ -7,122 +7,120 @@ package umcg.genetica.console;
 import umcg.genetica.util.RunTimer;
 
 /**
- *
  * @author harmjan
  */
 public class ProgressBar {
 
-    private RunTimer timer;
-    private long iterations = 0;
-    private long maxIterations = 0;
-    private int printEvery = 0;
-    private int width = 25;
-    private int maxwidth = 0;
-    private boolean isLong = false;
+	private RunTimer timer;
+	private long iterations = 0;
+	private long maxIterations = 0;
+	private int printEvery = 0;
+	private int width = 25;
+	private int maxwidth = 0;
+	private boolean isLong = false;
 
-    public ProgressBar(long length) {
-        iterations = 0;
+	public ProgressBar(long length) {
+		iterations = 0;
 
-        isLong = true;
-        maxIterations = length;
-        printEvery = (int) Math.ceil((double) length / width);
-        timer = new RunTimer();
+		isLong = true;
+		maxIterations = length;
+		printEvery = (int) Math.ceil((double) length / width);
+		timer = new RunTimer();
 
-        System.out.println("\nProgress:");
+		System.out.println("\nProgress:");
 
-        String out = "|";
-        for (int i = 0; i < width - 1; i++) {
-            out += "-";
-        }
-        out += "| Waiting for update...\r";
-        maxwidth = out.length();
-        System.out.print(out);
-    }
+		String out = "|";
+		for (int i = 0; i < width - 1; i++) {
+			out += "-";
+		}
+		out += "| Waiting for update...\r";
+		maxwidth = out.length();
+		System.out.print(out);
+	}
 
-    public ProgressBar(long length, String title) {
-        iterations = 0;
-        maxIterations = length;
-        printEvery = (int) Math.ceil((double) length / width);
-        timer = new RunTimer();
+	public ProgressBar(long length, String title) {
+		iterations = 0;
+		maxIterations = length;
+		printEvery = (int) Math.ceil((double) length / width);
+		timer = new RunTimer();
 
-        System.out.println("\n" + title);
+		System.out.println("\n" + title);
 
-        String out = "|";
-        for (int i = 0; i < width - 1; i++) {
-            out += "-";
-        }
-        out += "| Waiting for update...\r";
+		String out = "|";
+		for (int i = 0; i < width - 1; i++) {
+			out += "-";
+		}
+		out += "| Waiting for update...\r";
 
-        maxwidth = out.length();
-        System.out.print(out);
-    }
+		maxwidth = out.length();
+		System.out.print(out);
+	}
 
-    public void iterate() {
-        iterations++;
-        if (iterations % printEvery == 0) {
-            print();
-        }
-    }
+	public void iterate() {
+		iterations++;
+		if (iterations % printEvery == 0) {
+			print();
+		}
+	}
 
-    public void set(long num) {
-        iterations = num;
-        print();
-    }
+	public void set(long num) {
+		iterations = num;
+		print();
+	}
 
-    public void print() {
+	public void print() {
 
-        if (printEvery > 0) {
-            StringBuilder out = new StringBuilder(maxwidth);
-            int numToPrint = (int) Math.ceil(iterations / (double) printEvery);
-            if (numToPrint > width) {
-                numToPrint = width;
-            }
-            out.append("|");
-            for (int i = 0; i < numToPrint; i++) {
-                out.append("#");
-            }
-            for (int i = 0; i < width - numToPrint - 1; i++) {
-                out.append("-");
-            }
-            out.append("| ");
-            int perc = (int) Math.ceil((double) iterations / maxIterations * 100);
+		if (printEvery > 0) {
+			StringBuilder out = new StringBuilder(maxwidth);
+			int numToPrint = (int) Math.ceil(iterations / (double) printEvery);
+			if (numToPrint > width) {
+				numToPrint = width;
+			}
+			out.append("|");
+			for (int i = 0; i < numToPrint; i++) {
+				out.append("#");
+			}
+			for (int i = 0; i < width - numToPrint - 1; i++) {
+				out.append("-");
+			}
+			out.append("| ");
+			int perc = (int) Math.ceil((double) iterations / maxIterations * 100);
 
-            out.append(perc).append("% - T: ").append(timer.getTimeDesc());
+			out.append(perc).append("% - T: ").append(timer.getTimeDesc());
 
-            long diff = timer.getTimeDiff() / 1000000000;
-            double timePerIter = (double) diff / iterations;
-            double timeLeft = timePerIter * (maxIterations - iterations);
-            String strTimeLeft = timer.getTimeDesc(((long) timeLeft) * 1000000000);
-            out.append(" T-: ").append(strTimeLeft);
-            out.append(" #: ").append(iterations).append("/").append(maxIterations);
-
-
-
-            int length = out.toString().length();
-            if (length < maxwidth) {
-                int difflen = maxwidth - out.length();
-                if (difflen > 0) {
-                    int i = 0;
-                    while (i < difflen) {
-                        out.append(" ");
-                        i++;
-                    }
-                }
-            } else {
-                maxwidth = length;
-            }
+			long diff = timer.getTimeDiff() / 1000000000;
+			double timePerIter = (double) diff / iterations;
+			double timeLeft = timePerIter * (maxIterations - iterations);
+			String strTimeLeft = timer.getTimeDesc(((long) timeLeft) * 1000000000);
+			out.append(" T-: ").append(strTimeLeft);
+			out.append(" #: ").append(iterations).append("/").append(maxIterations);
 
 
-            out.append("\r");
-            System.out.print(out.toString());
-        }
+			int length = out.toString().length();
+			if (length < maxwidth) {
+				int difflen = maxwidth - out.length();
+				if (difflen > 0) {
+					int i = 0;
+					while (i < difflen) {
+						out.append(" ");
+						i++;
+					}
+				}
+			} else {
+				maxwidth = length;
+			}
 
-    }
 
-    public void close() {
-        iterations = maxIterations;
-        print();
-        System.out.println("");
+			out.append("\r");
+			System.out.print(out.toString());
+		}
 
-    }
+	}
+
+	public void close() {
+		iterations = maxIterations;
+		print();
+		System.out.println("");
+
+	}
 }
