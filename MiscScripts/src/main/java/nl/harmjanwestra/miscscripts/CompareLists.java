2b14518492812c6c54afc812882833c502b7b3ca
Harm-Jan Westra <westra.harmjan@outlook.com>
2016-06-30 20:24:01 -0400

    Correlate samples between markers for a given set of variants

---
+++ MiscScripts/src/main/java/nl/harmjanwestra/miscscripts/CompareLists.java
@@ -0,0 +1,45 @@
+package nl.harmjanwestra.miscscripts;
+
+import umcg.genetica.io.text.TextFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Created by hwestra on 6/30/16.
+ */
+public class CompareLists {
+
+	public static void main(String[] args) {
+		try {
+
+			CompareLists c = new CompareLists();
+			c.run("/Data/tmp/2016-06-29-quals/samples/RA-assoc0.3-COSMO-chr8-samplelist.txt",
+					"/Data/tmp/2016-06-29-quals/samples/T1D-assoc0.3-COSMO-chr8-samplelist.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run(String file1, String file2) throws IOException {
+		TextFile tf = new TextFile(file1, TextFile.R);
+		ArrayList<String> samples1 = tf.readAsArrayList();
+		tf.close();
+
+		TextFile tf2 = new TextFile(file2, TextFile.R);
+		ArrayList<String> samples2 = tf2.readAsArrayList();
+		tf.close();
+
+		HashSet<String> samples1hash = new HashSet<String>();
+		samples1hash.addAll(samples1);
+
+		int shared = 0;
+		for (String s : samples2) {
+			if (samples1hash.contains(s)) {
+				shared++;
+			}
+		}
+		System.out.println(shared);
+	}
+}
